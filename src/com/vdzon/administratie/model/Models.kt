4f179f4c4002a09652022a58f892210c4ddd2a68
robbertvdzon <robbert@vdzon.com>
2017-03-28 20:34:06 +0200

    meer voorbeelden

--- src/com/vdzon/administratie/model/Models.kt
+++
@@ -1,5 +0,0 @@
-package com.vdzon.administratie.model
-
-data class Contact(val naam:String, val adres:Adres?)
-
-data class Adres(val woonplaats:String?, val straat:String?, val huisnummer:String?)
